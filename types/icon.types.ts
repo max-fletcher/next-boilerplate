d1d382101b551a5bae51e6843950945b9ed4553f
Mahin Chowdhury <54007446+max-fletcher@users.noreply.github.com>
2025-04-08 16:26:27 +0600

    Got rid of CustomButton since its not needed. SVG works.

---
+++ types/icon.types.ts
@@ -0,0 +1,15 @@
+import { ReactNode } from "react"
+
+export type TButtonProps = {
+  classes?: string
+  Icon?: ReactNode | null,
+  text?: string,
+  disabled?: boolean,
+  rest?: any;
+}
+
+export type TIconTypes = {
+  classes?: string,
+  width?: number,
+  height?: number,
+}
